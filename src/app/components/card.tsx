8f7732a40286f39599d333c2348ea6abff5f78a3
Glory Ayanwola <89777377+Gloking-Glory@users.noreply.github.com>
2025-07-28 01:04:50 +0100

    Update card countryname

--- src/app/components/card.tsx
+++ src/app/components/card.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { FC } from "react";
 interface CardProps {
-  title: string;
+  countryName: string;
   price?: string;
   children?: React.ReactNode;
 }
 
-export const Card: FC<CardProps> = ({ title, price, children }) => (
+export const Card: FC<CardProps> = ({ countryName, price, children }) => (
   <div className="bg-white rounded-lg shadow-md overflow-hidden border">
     <div className="flex">
       <div className="p-4 flex-1">
-        <h3 className="font-semibold text-lg">{title}</h3>
+        <h3 className="font-semibold text-lg">{countryName}</h3>
         {price && <p className="text-blue-600 mt-2 font-bold">{price}</p>}
         {children}
       </div>
